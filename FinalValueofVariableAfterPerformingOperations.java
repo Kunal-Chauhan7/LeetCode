90980837e88721a0cbb3fe9a0dcd661e5ac2f320
Kunal Chauhan <kunalchauhanlion@gmail.com>
2024-07-24 22:52:07 +0530

    done with a question

---
+++ FinalValueofVariableAfterPerformingOperations.java
@@ -0,0 +1,14 @@
+package kunal;
+	
+public class FinalValueofVariableAfterPerformingOperations {
+	public int finalValueAfterOperations(String[] operations) {
+		int x = 0;
+		for(String s:operations) {
+			if(s.equals("X++")||s.equals("++X"))
+				x++;
+			else
+				x--;
+		}
+		return x;
+    }
+}
---
+++ FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,14 @@
+package kunal;
+
+public class FlattenBinaryTreetoLinkedList {
+	private TreeNode prev = null;
+	public void flatten(TreeNode root) {
+		if(root == null)
+			return;
+		flatten(root.right);
+		flatten(root.left);
+		root.left = null;
+		root.right = prev;
+		prev = root;
+    }
+}
