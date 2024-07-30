84a789862a9d538d3b570a4cdf36fbeebc07a8ac
Kunal Chauhan <kunalchauhanlion@gmail.com>
2024-07-31 03:06:03 +0530

    done with Minimum Deletions to Make String Balanced

---
+++ MinimumDeletionstoMakeStringBalanced.java
@@ -0,0 +1,18 @@
+public class MinimumDeletionstoMakeStringBalanced {
+    public int minimumDeletions(String s) {
+        int b = 0;
+        int ans = 0;
+        for (char c : s.toCharArray()){
+            if(c=='b')b++;
+            else{
+                if(b>0){
+                    ans+=1;
+                    b--;
+                }
+                else
+                b=0;
+            }
+        }
+        return ans;
+    }
+}
--- MinimumTimetoMakeRopeColorful.java
+++ MinimumTimetoMakeRopeColorful.java
@@ -1,5 +1,27 @@
 package kunal;
 
 public class MinimumTimetoMakeRopeColorful {
-
+	public int minCost(String colors, int[] neededTime) {
+	    int ans = 0;
+	    int n = colors.length();	    
+	    for (int i = 1; i < n; i++) {
+	        if (colors.charAt(i) == colors.charAt(i - 1)) {
+	            int start = i - 1;
+	            while (i < n && colors.charAt(i) == colors.charAt(start)) {
+	                i++;
+	            }
+	            int maxTime = neededTime[start];
+	            int sumTime = 0;
+	            for (int j = start; j < i; j++) {
+	                sumTime += neededTime[j];
+	                if (neededTime[j] > maxTime) {
+	                    maxTime = neededTime[j];
+	                }
+	            }
+	            ans += (sumTime - maxTime);
+	            i--;
+	        }
+	    }
+	    return ans;
+	}
 }
