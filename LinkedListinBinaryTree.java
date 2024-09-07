d04f36b9241f33a86011b55a70bfe933cec42f48
Kunal Chauhan <kunalchauhanlion@gmail.com>
2024-09-07 17:33:24 +0530

    solved Linked List in Binary Tree

---
+++ LinkedListinBinaryTree.java
@@ -0,0 +1,18 @@
+package kunal;
+
+public class LinkedListinBinaryTree {
+	public boolean isSubPath(ListNode head, TreeNode root) {
+		if(head==null)
+			return true;
+		if(root==null)
+			return false;
+		return dfs(head,root) || (isSubPath(head, root.left) || isSubPath(head, root.right));
+    }
+	public boolean dfs(ListNode head, TreeNode root) {
+		if(head==null)
+			return true;
+		if(root == null)
+			return false;
+		return head.val==root.val && (dfs(head.next,root.left)||dfs(head.next,root.right));
+	}
+}
