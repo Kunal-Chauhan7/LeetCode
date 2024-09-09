a47cc8d04d6f4f5114e262c331a09c5fe298af57
Kunal Chauhan <kunalchauhanlion@gmail.com>
2024-09-10 02:36:39 +0530

    solved spiral matrix 4

--- LongestPalindromicSubsequence.java
+++ LongestPalindromicSubsequence.java
@@ -1,20 +1,36 @@
 package kunal;
 
+import java.util.Arrays;
+
 public class LongestPalindromicSubsequence {
 	public static int longestPalindromeSubseq(String s) {
-		if(s == null)
-			return 0;
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
         StringBuilder str1 = new StringBuilder(s);
         StringBuilder str2 = new StringBuilder(s).reverse();
-        return longestPalindromeSubseq(str1, str2, 0, 0);
+        int[][] dp = new int[s.length()][s.length()];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return longestPalindromeSubseq(str1, str2, 0, 0, dp);
     }
-	public static int longestPalindromeSubseq(StringBuilder str1 , StringBuilder str2 , int i , int j) {
-		if(str1.length() == i || str2.length()==j)
-			return 0;
-		if(str1.charAt(i) == str2.charAt(j))
-			return 1 + longestPalindromeSubseq(str1, str2, i+1, j+1);
-		return Math.max(longestPalindromeSubseq(str1, str2, i, j+1), longestPalindromeSubseq(str1, str2, i+1, j));
-	}
+
+    public static int longestPalindromeSubseq(StringBuilder str1, StringBuilder str2, int i, int j, int[][] dp) {
+        if (i == str1.length() || j == str2.length()) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (str1.charAt(i) == str2.charAt(j)) {
+            dp[i][j] = 1 + longestPalindromeSubseq(str1, str2, i + 1, j + 1, dp);
+        } else {
+            dp[i][j] = Math.max(longestPalindromeSubseq(str1, str2, i, j + 1, dp),
+                               longestPalindromeSubseq(str1, str2, i + 1, j, dp));
+        }
+        return dp[i][j];
+    }
 	public static void main(String[] args) {
 		System.out.println(longestPalindromeSubseq("bbbab"));
 	}
--- SpiralMatrixII.java
+++ SpiralMatrixII.java
@@ -1,5 +1,49 @@
 package kunal;
 
-public class SpiralMatrixII {
+import java.util.Arrays;
 
+public class SpiralMatrixII {
+	public int[][] spiralMatrix(int m, int n, ListNode head) {
+	    int left = 0, right = n - 1, top = 0, bottom = m - 1;
+	    int[][] arr = new int[m][n];
+	    for (int[] row : arr) {
+	        Arrays.fill(row, -1);
+	    }
+	    
+	    ListNode temp = head;
+	    while (temp != null && top <= bottom && left <= right) {
+	        // Traverse from left to right
+	        for (int i = left; i <= right; i++) {
+	            if (temp == null) return arr;
+	            arr[top][i] = temp.val;
+	            temp = temp.next;
+	        }
+	        top++;
+	        
+	        // Traverse from top to bottom
+	        for (int i = top; i <= bottom; i++) {
+	            if (temp == null) return arr;
+	            arr[i][right] = temp.val;
+	            temp = temp.next;
+	        }
+	        right--;
+	        
+	        // Traverse from right to left
+	        for (int i = right; i >= left; i--) {
+	            if (temp == null) return arr;
+	            arr[bottom][i] = temp.val;
+	            temp = temp.next;
+	        }
+	        bottom--;
+	        
+	        // Traverse from bottom to top
+	        for (int i = bottom; i >= top; i--) {
+	            if (temp == null) return arr;
+	            arr[i][left] = temp.val;
+	            temp = temp.next;
+	        }
+	        left++;
+	    }   
+	    return arr;
+	}
 }
