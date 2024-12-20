4511c3e8dc3afacf0ef578b1a30da0dc693855ef
Kunal Chauhan <kunalchauhanlion@gmail.com>
2024-12-21 02:35:37 +0530

    solved  Reverse Odd Levels of Binary Tree

---
+++ ReverseOddLevelsofBinaryTree.java
@@ -0,0 +1,21 @@
+package kunal;
+
+public class ReverseOddLevelsofBinaryTree {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        if (root == null)
+            return root;
+        reverseOddLevelsHelper(root.left, root.right, 1);
+        return root;
+    }
+    private void reverseOddLevelsHelper(TreeNode left, TreeNode right, int level) {
+        if (left == null || right == null)
+            return;
+        if (level % 2 == 1) {
+            int temp = left.val;
+            left.val = right.val;
+            right.val = temp;
+        }
+        reverseOddLevelsHelper(left.left, right.right, level + 1);
+        reverseOddLevelsHelper(left.right, right.left, level + 1);
+    }
+}
